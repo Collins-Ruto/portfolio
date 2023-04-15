afe26ca07ec9601f085b6302026715eaac352ecd
collins-ruto <collinsruto48@gmail.com>
2023-04-15 18:51:06 +0300

    update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from "react";
-import homepic from "../res/homepic1.webp";
+import homepic from "~/assets/homepic1.webp";
 import { Button } from "~/components";
 import {  useNavigate } from "next/link";
 import Image from "next/image";
