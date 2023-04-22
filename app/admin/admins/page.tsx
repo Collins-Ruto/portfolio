b1e5a6ee0c142798520e6ad9c6f8c336c3876391
collins-ruto <collinsruto48@gmail.com>
2023-04-22 18:06:41 +0300

    post requsts

---
+++ app/admin/admins/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Admins = () => {
+  return (
+    <div>Admins</div>
+  )
+}
+
+export default Admins
---
+++ app/admin/streams/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Streams = () => {
+  return (
+    <div>Streams</div>
+  )
+}
+
+export default Streams
