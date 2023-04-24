66c0e78e904cc8d9bfab2d853bd2a2fce029de80
collins-ruto <collinsruto48@gmail.com>
2023-04-24 13:22:21 +0300

    exam ssr

--- app/student/exams/page.tsx
+++ app/student/exams/page.tsx
@@ -1,36 +1,21 @@
-'use client'
-import React, { useEffect } from "react";
+import React from "react";
 import { Loader } from "~/components";
-import {  DummyUser, Subjects } from '~/api/types';
-import type { User, Result } from '~/api/types';
+import { DummyUser, Subjects } from "~/api/types";
+import type { User, Result } from "~/api/types";
 import { api } from "@/utils/api";
 import type { Exam } from "@prisma/client";
 
 function Exam() {
-  useEffect(() => {
-    
-    //  setUserType(user)
-    // axios
-    //   .get(`https://lmsadmin.onrender.com/exams/studentexams?slug=${user.slug}`)
-    //   .then((res) => {
-    //     setExam(res.data.student.exams);
-    //   });
-
-    // axios.get("https://lmsadmin.onrender.com/infos").then((res) => {
-    //   setSubjects(res.data.subjects);
-    //   setLoading(false);
-    // });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const id = "641dd16f2eece6ce9587cb0d"
+  const id = "641dd16f2eece6ce9587cb0d";
 
   const userFromLocalStorage = localStorage.getItem("user");
-  const user: User = userFromLocalStorage !== null ? JSON.parse(userFromLocalStorage) as User : DummyUser
+  const user: User =
+    userFromLocalStorage !== null
+      ? (JSON.parse(userFromLocalStorage) as User)
+      : DummyUser;
 
-  const {data, isLoading, error} = api.exam.studentExams.useQuery(id);
-  //  const [exam, setExam] = useState<Exam[]>(data);
-  const exams: Exam[] | undefined = data
+  const { data, isLoading, error } = api.exam.studentExams.useQuery(id);
+  const exams: Exam[] | undefined = data;
   console.log("exam", exams);
   console.log("exam user", user);
   console.log("exam error", error);
@@ -47,42 +32,44 @@ function Exam() {
             <h1>Sorry, Your results have not been uploaded yet</h1>
           </div>
         ) : (
-          <div className="m-4 bg-[#F7F6FB] rounded-xl p-4 overflow-auto">
+          <div className="m-4 overflow-auto rounded-xl bg-[#F7F6FB] p-4">
             <table className=" w-full text-justify">
               <thead>
-                <tr className="text-lg p-4">
+                <tr className="p-4 text-lg">
                   <th className="p-4">Exam</th>
                   <th className="p-4">Term</th>
                   <th className="p-4">Date</th>
                   {Subjects.map((subject, index) => (
-                    <th className="p-4 border-x-2" key={index}>
+                    <th className="border-x-2 p-4" key={index}>
                       {subject.slug}
                     </th>
                   ))}
                 </tr>
               </thead>
               <tbody>
-                {
-                  exams?.map((exam, index) => {
-                    return (
-                      <tr
-                        className={` p-4 ${index % 2 === 0 ? "bg-white" : ""}`}
-                        key={index}
-                      >
-                        <td className="p-4">{exam?.name}</td>
-                        <td className="p-4">{exam?.term}</td>
-                        <td className="p-4">{exam.examDate}</td>
-                        {Subjects.map((subject, index) => {
-                          const resultsObj: Result[] = exam.results as Result[]
-                          const myResult = resultsObj.find(obj => obj.slug === subject.slug);
-                          return (
-                          <td className="p-4 border-x-2" key={index}>
-                              {myResult?.marks || "-"}
+                {exams?.map((exam, index) => {
+                  return (
+                    <tr
+                      className={` p-4 ${index % 2 === 0 ? "bg-white" : ""}`}
+                      key={index}
+                    >
+                      <td className="p-4">{exam?.name}</td>
+                      <td className="p-4">{exam?.term}</td>
+                      <td className="p-4">{exam.examDate}</td>
+                      {Subjects.map((subject, index) => {
+                        const resultsObj: Result[] = exam.results as Result[];
+                        const myResult = resultsObj.find(
+                          (obj) => obj.slug === subject.slug
+                        );
+                        return (
+                          <td className="border-x-2 p-4" key={index}>
+                            {myResult?.marks || "-"}
                           </td>
-                        )})}
-                      </tr>
-                    );
-                  })}
+                        );
+                      })}
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           </div>
