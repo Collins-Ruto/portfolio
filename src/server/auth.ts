360f1a9ef23a4eb2ed804bf8dc2ac0ddf049e351
collins-ruto <collinsruto48@gmail.com>
2023-07-06 17:17:29 +0300

    ¯\_(ツ)_/¯

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -37,10 +37,8 @@ declare module "next-auth" {
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      // @ts-ignore
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-      // @ts-ignore
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET ?? "",
     }),
     /**
      * ...add more providers here.
