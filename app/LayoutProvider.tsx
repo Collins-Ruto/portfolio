2928d40c0b69f4b5f9667b6ee647c9de08a7a948
collins-ruto <collinsruto48@gmail.com>
2026-01-19 08:55:21 +0300

    separate home from global layouts

--- app/LayoutProvider.tsx
+++ app/LayoutProvider.tsx
@@ -1,9 +1,33 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { usePathname } from "next/navigation";
 import { Footer, Header } from "~/components";
 
 export const LayoutProvider = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+
+  // Define pages where styles/provider should NOT apply
+  const excludedPaths = ["/home2", "/signup", "/admin/dashboard"];
+  const isExcluded = excludedPaths.includes(pathname || "") || pathname === "/";
+
+  if (isExcluded) {
+    return (
+      <>
+        <ThemeProvider enableSystem={true} attribute="class">
+          <div
+            className={` flex min-h-[100vh] flex-col justify-between bg-gray-100 dark:bg-slate-900`}
+          >
+            <Header />
+            <div className="flex">
+              <div className="w-full">{children}</div>
+            </div>
+            <Footer />
+          </div>
+        </ThemeProvider>
+      </>
+    ); // Return children without any wrapper styles
+  }
   return (
     <>
       <ThemeProvider enableSystem={true} attribute="class">
