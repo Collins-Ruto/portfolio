2467db6ac6f051ca296bb41cb586bac5eefaee36
collins-ruto <collinsruto48@gmail.com>
2026-02-06 15:58:20 +0300

    refactor(projects): moved to newer project card design for projects section
    - large cover photo images

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,6 +4,7 @@ import projects from "~/assets/sortedArray2.json";
 import pinnedProjects2 from "~/assets/pinnedProjects2.json";
 import ThemeToogle from "~/components/ThemeToogle";
 import { PinnedCard, ProjectCard } from "~/components";
+import { PinnedCard2 } from "~/components/ProjectCards";
 
 export const metadata: Metadata = {
   title: "Projects",
@@ -15,8 +16,15 @@ const Page = () => {
       <div className="flex min-h-screen flex-col items-center justify-between">
         <main className=" md: py-4">
           <div className="flex items-center justify-between">
-            <h1 className="py-6 text-5xl font-bold md:py-16">My Crafts</h1>
+            <h1 className="py-6 text-5xl font-bold md:py-16">Projects</h1>
             <ThemeToogle place="projects-toogle" />
+          </div>
+          <div className="md:flex-nowra mb-4 flex flex-wrap gap-4 sm:flex-row">
+            {pinnedProjects2.map((project, index) => (
+              <div className="w-1/2 grow md:w-1/3 lg:w-1/4" key={index}>
+                <PinnedCard2 project={project} />
+              </div>
+            ))}
           </div>
           <div className="md:flex-nowra mb-4 flex flex-wrap gap-4 sm:flex-row">
             {pinnedProjects2.map((project, index) => (
